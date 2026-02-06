1ba4fba7ad37368a7f1580cf2ac72f6b8f8febf7
Intern <contact@accelorindia.com>
2026-02-06 17:31:10 +0530

    test + create project f+b

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const { PrismaPg } = require('@prisma/adapter-pg');
 const { createAllEmployeesRouter } = require('./routers/User_Management/AllEmployees.routes');
 const { createAuthRouter } = require('./routers/User_Management/auth.routes');
 
-const {createUutRoutes} = require('./routers/UutRecords/uutRecord.Routes');
 const { createTestRequestRouter } = require('./routers/TestRequestAndProjectDetails/testRequest.Routes');
 const { createUutRoutes } = require('./routers/UutRecords/uutRecord.Routes');
 const createProjectRoutes = require('./routers/ProjectRoute/projectRoutes');
